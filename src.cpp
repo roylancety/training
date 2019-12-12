61d7746dea7422cd3a756d4ca1c69f6e7d67058a
Tyson Roylance <tysonlr@login01.rc.byu.edu>
2019-12-12 13:18:01 -0700

    yay

--- src.cpp
+++ src.cpp
@@ -1,34 +1,32 @@
 #include<iostream>
 #include<fstream>
-#include<cmath>}
+#include<cmath>
 
-
-
+using namespace std;
 
 int main()
 {
-    
+   // float x;
+   // float t;
     float dt  = 0.0;
     int  numSteps  = 0;
     double dx_dt;
-    double x;
     double dx;
-    double t;
     ifstream infile;
     infile.open("params.dat");
     infile >>  dt  >> numSteps;
     infile.close();
 
 
-    float x = 0;
-    float t = 1;
+    float x = 1;
+    float t = 0;
  
 
     for(int i = 0; i < numSteps; i++)
     {
           dx_dt = -3*x;
-          dx = dx_dt * dt;
-          x = x + dx;
+         // dx = dx_dt * dt;
+          x =(1- 3*dt)*x;
           t = t + dt;
           cout << t << " " << x << endl;
     }	
